14aa26bade355f044b8b6dd8814a80724b5b60d1
OxaDefrizal <oxadefrizal@students.amikom.ac.id>
2023-05-08 12:07:23 +0700

    kurang persentase

--- js/app.js
+++ js/app.js
@@ -1,3 +1,72 @@
-function inputNumber(param) {
-    document.getElementById("nomor1").innerHTML += param.innerText;
+let displayValue = '';
+
+function addToDisplay(value) {
+  displayValue += value;
+  document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = '';
+  document.getElementById('display').value = '';
+}
+
+function calculate() {
+  try {
+    const result = evaluateExpression(displayValue);
+    document.getElementById('display').value = result;
+    displayValue = result.toString();
+  } catch (error) {
+    // Tangani kesalahan jika terjadi (misalnya, ekspresi tidak valid)
+    console.log('Error:', error);
+  }
+}
+
+function toggleSign() {
+  if (displayValue !== '') {
+    const firstChar = displayValue.charAt(0);
+    if (firstChar === '-') {
+      displayValue = displayValue.slice(1); // Menghapus tanda negatif
+    } else {
+      displayValue = `-${displayValue}`; // Menambahkan tanda negatif
+    }
+    document.getElementById('display').value = displayValue;
+  }
+}
+
+function backspace() {
+  displayValue = displayValue.slice(0, -1); // Menghapus karakter terakhir
+  document.getElementById('display').value = displayValue;
+}
+
+function Percentage() {
+  const percentageInput = document.getElementById('percentageInput');
+  const percentage = parseFloat(percentageInput.value);
+
+  if (displayValue !== '' && !isNaN(percentage) && percentage !== 0) {
+    const operand = parseFloat(displayValue);
+    const result = operand / percentage;
+    displayValue = result.toString();
+    document.getElementById('display').value = displayValue;
+  }
+
+  percentageInput.value = ''; // Mengosongkan input persentase setelah dihitung
+  togglePercentageInput(); // Menyembunyikan input persentase setelah perhitungan
+}
+
+
+
+function evaluateExpression(expression) {
+  // Buat regex untuk mencocokkan karakter yang tidak diinginkan
+  const invalidCharsRegex = /[^-()\d/*+.]/g;
+
+  // Cek apakah ada karakter yang tidak valid dalam ekspresi
+  if (invalidCharsRegex.test(expression)) {
+    throw new Error('Invalid expression');
+  }
+
+  // Evaluasi ekspresi dengan menggunakan Function constructor
+  const evaluate = new Function(`return ${expression}`);
+
+  // Panggil fungsi evaluate dan kembalikan hasilnya
+  return evaluate();
 }
